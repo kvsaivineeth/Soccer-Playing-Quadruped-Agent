2d06e0db765738897330ee408a9670795592fcc1
Ankur Gupta <rukna1000@gmail.com>
2019-11-28 14:55:27 -0500

    TRPO wireframe is set up.
    Lol just have to implement it tho

--- scripts/algo/trpo/TRPO.py
+++ scripts/algo/trpo/TRPO.py
@@ -13,22 +13,52 @@
 #     name: python3
 # ---
 
+# + [markdown] toc-hr-collapsed=false
 # # TRPO Implementation
 #
 # Try to create a basic policy to get the agent to try to kick the ball to the target. The paper for this algorithm can be found [here](https://arxiv.org/pdf/1502.05477.pdf).
 
+# + [markdown] toc-hr-collapsed=false
 # ## Setup
 # Hyperparameters and other preliminaries.
 #
 # ### Imports
 
+# +
 from dm_control import suite
 from dm_control import viewer
 import numpy as np
+
 import torch
+# -
 
 # ### Constants
 
-# Get the training device
+# Get the training device and dynamically set it to the GPU if needed.
+
+_DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 
+# ## Define observation and agent inputs
+
+def to_input(obs):
+  pass
+
+
+# ## Define reward function
+
+def reward(physics):
+  pass
+
+
+# ## Create the environment
+
+# +
+task_kwargs = {
+  'reward_func': reward
+}
+
+env = suite.load(domain_name="quadruped", 
+                 task_name="soccer", 
+                 visualize_reward=True, 
+                 task_kwargs=task_kwargs)
