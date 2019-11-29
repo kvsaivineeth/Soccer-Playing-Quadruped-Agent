4dac3b7321cf38230185bb9e35a95ee5c32acbe8
Ankur Gupta <rukna1000@gmail.com>
2019-11-28 21:22:38 -0500

    Convert the observation and networks sizes. * Dynamcally determine the inputs and outputs based on the env. * Exact method to convert the observation might have to be changed based on how pytorch works

--- scripts/algo/trpo/TRPO.py
+++ scripts/algo/trpo/TRPO.py
@@ -36,19 +36,54 @@ import torch
 
 # Get the training device and dynamically set it to the GPU if needed.
 
+# +
+# Computational device 
 _DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
+# Cardinalities
+_walls_c = 3
+_num_walls = 4
+_ball_state_c = 9
+_egocentric_state_c = 44
+_INPUT_DIM = _walls_c * _num_walls + _ball_state_c + _egocentric_state_c
+
+
+# -
 
 # ## Define observation and agent inputs
+#
+# Here, an agent observation is converted into the input for TRPO. The observed features that are used are: 
+# * Wall vectors for the left, right, top, and back walls of the goal
+# * The ball x,y,z positions and velocicties relative to the agent
+# * The state of the agent itself (joints, etc)
+#
+# The features are converted to be 1-dimensional and then concatenated as follows:
+# $$\left[ \matrix{ left \cr
+#                   right \cr
+#                   top \cr
+#                   back \cr
+#                   ball-state \cr
+#                   egocentric-state} \right]$$
 
 def to_input(obs):
-  pass
+  left, right, top, back = obs['goal_walls_positions']
+  ball_state = obs['ball_state']
+  egocentric_state = obs['egocentric_state']
+  
+  return np.concatenate((
+    left.ravel(),
+    right.ravel(),
+    top.ravel(),
+    back.ravel(),
+    ball_state.ravel(),
+    egocentric_state.ravel()
+  ))
 
 
 # ## Define reward function
 
 def reward(physics):
-  pass
+  return 0
 
 
 # ## Create the environment
@@ -62,3 +97,18 @@ env = suite.load(domain_name="quadruped",
                  task_name="soccer", 
                  visualize_reward=True, 
                  task_kwargs=task_kwargs)
+# -
+
+# Get the dynamic output required for TRPO
+
+_OUTPUT_DIM = env.action_spec().shape
+
+#
+
+timestep = env.reset()
+
+to_input(timestep.observation)
+
+env.action_spec().shape
+
+
